f5b860c83b1334dc00184aea2bcae5c53652a528
Adam Koncz <adam.koncz@vacuumlabs.com>
2021-02-17 11:18:33 +0100

    Add basic kotlin & spring-boot app

---
+++ app/src/main/kotlin/hu/konczdam/btcexchange/BtcexchangeApplication.kt
@@ -0,0 +1,11 @@
+package hu.konczdam.btcexchange
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BtcexchangeApplication
+
+fun main(args: Array<String>) {
+	runApplication<BtcexchangeApplication>(*args)
+}
---
+++ app/src/test/kotlin/hu/konczdam/btcexchange/BtcexchangeApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.konczdam.btcexchange
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BtcexchangeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
